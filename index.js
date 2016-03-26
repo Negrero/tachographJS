c0a33f64d078ec99d1ab3ec133632a0370f7389f
negrero <valdepeace@gmail.com>
2016-03-26 14:12:17 +0100

    integration vu and card

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * Created by Andres Carmona Gil on 26/08/2015.
  */
-'use strict'
+'use strict';
 //var FileTGD=require('./file/FileTGD')
 //var Vu=require('./vu/vuBlock')
-require('./lib/vu/size')
-require('./lib/card/size')
-var Vu=require('./lib/vu/vuBlock').vuBlock
+require('./lib/vu/size');
+require('./lib/card/size');
+var Vu=require('./lib/vu/vuBlock').vuBlock;
 //var Card=require('./lib/card/cardBlock').cardBlock
-var card=require('./lib/card/cardBlockTuned')
+var card=require('./lib/card/cardBlockTuned');
 /*
  * module.exports
  * Export callback json of file tachograf
@@ -18,10 +18,10 @@ var card=require('./lib/card/cardBlockTuned')
 function tgd(buffer){
 
     if(buffer[0]==0x76){
-        //var f=new Vu(buffer)
-        card(buffer)
+       // var f=new Vu(buffer)
+        card(buffer);
     }else{
-        card(buffer)
+        card(buffer);
     }
 
 }
@@ -29,11 +29,11 @@ function tgd(buffer){
 
 
 function FileTGD(buffer){
-    this.buffer=buffer
+    this.buffer=buffer;
 }
 
 
-module.exports = tgd
+module.exports = tgd;
 
 
 
